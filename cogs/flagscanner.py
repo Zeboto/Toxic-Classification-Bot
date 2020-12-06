d9ef6593aa6a4d6c016b649889c2849ecbd09ef6
Zeboto <zebrawii@gmail.com>
2020-12-05 23:53:16 -0500

    Move clean_text method to flagscanner

--- cogs/flagscanner.py
+++ cogs/flagscanner.py
@@ -60,7 +60,23 @@ class FlagScanner(commands.Cog):
                 for flag in flags:
                     await self.bot.get_channel(self.bot.config.get('flag_channel')).send(embed=flag)
                 
-
+    def clean_text(self, text):
+        text = text.lower()
+        text = re.sub(r"what's", "what is ", text)
+        text = re.sub(r"\'s", " ", text)
+        text = re.sub(r"\'ve", " have ", text)
+        text = re.sub(r"can't", "cannot ", text)
+        text = re.sub(r"n't", " not ", text)
+        text = re.sub(r"i'm", "i am ", text)
+        text = re.sub(r"\'re", " are ", text)
+        text = re.sub(r"\'d", " would ", text)
+        text = re.sub(r"\'ll", " will ", text)
+        text = re.sub(r"\'scuse", " excuse ", text)
+        text = re.sub('\W', ' ', text)
+        text = re.sub('\s+', ' ', text)
+        text = text.strip(' ')
+        return text
+        
     def compute_messages(self):
         
         test_messages = self.messages.copy()
--- cogs/utils.py
+++ cogs/utils.py
@@ -9,19 +9,3 @@ def check_granted_server(ctx):
 def in_scan_channel(ctx):
     return ctx.channel.id in ctx.bot.config.get("scan_channels", [])
 
-def clean_text(self, text):
-        text = text.lower()
-        text = re.sub(r"what's", "what is ", text)
-        text = re.sub(r"\'s", " ", text)
-        text = re.sub(r"\'ve", " have ", text)
-        text = re.sub(r"can't", "cannot ", text)
-        text = re.sub(r"n't", " not ", text)
-        text = re.sub(r"i'm", "i am ", text)
-        text = re.sub(r"\'re", " are ", text)
-        text = re.sub(r"\'d", " would ", text)
-        text = re.sub(r"\'ll", " will ", text)
-        text = re.sub(r"\'scuse", " excuse ", text)
-        text = re.sub('\W', ' ', text)
-        text = re.sub('\s+', ' ', text)
-        text = text.strip(' ')
-        return text
