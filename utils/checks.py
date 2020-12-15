3e1cf6c89146ee5435f2af23fc4b3a1372658aea
Zeboto <zebrawii@gmail.com>
2020-12-15 00:11:17 -0500

    Update checks

--- utils/checks.py
+++ utils/checks.py
@@ -1,6 +1,5 @@
 def check_granted_server(ctx):
-    allowed_channels = map(ctx.bot.get_channel, ctx.bot.config.get("scan_channels", []))
-    return ctx.guild in set([channel.guild for channel in allowed_channels if channel])
+    return ctx.guild.id == ctx.bot.config.get("bot_server")
 
 def in_scan_channel(obj, ctx):
     return ctx.channel.id in obj.bot.config.get("scan_channels", [])
