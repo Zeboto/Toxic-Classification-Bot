85bdf9cc9b4cc5335bb21dc95085db9533f52fa2
Zeboto <zebrawii@gmail.com>
2020-12-22 18:53:28 -0500

    Add max review queue size

--- cogs/review.py
+++ cogs/review.py
@@ -128,7 +128,8 @@ class ReviewQueue(commands.Cog):
                 return
             for r in new_reviews:
                 r['message'] = nlp_cog.clean_text(r['message'].content if type(r['message']) is not str else r['message'])
-            self.review_queue = new_reviews + self.review_queue
+            if len(self.review_queue) < self.bot.config.get('queue_length'):
+                self.review_queue = new_reviews + self.review_queue
             if len(self.in_review) < max_reviews_size and len(self.review_queue) > 0:    
                 for i in range(min(max_reviews_size-len(self.in_review),len(self.review_queue))):
                     new_review = self.review_queue.pop()
