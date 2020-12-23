5f12a818f8cacc4e433c3d9735eab32c28a3d643
Zeboto <zebrawii@gmail.com>
2020-12-22 20:36:51 -0500

    Move max in review queue size to config

--- cogs/review.py
+++ cogs/review.py
@@ -120,7 +120,7 @@ class ReviewQueue(commands.Cog):
         return review_message
 
     async def add_reviews_to_queue(self, new_reviews):
-        max_reviews_size = 50
+        max_reviews_size = self.bot.config.get('max_reviews_size')
         async with self.review_lock:
             nlp_cog = self.bot.get_cog('NLP')
             if nlp_cog is None:
