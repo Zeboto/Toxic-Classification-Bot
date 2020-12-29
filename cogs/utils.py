be96d9b1f1960eb2876661e938c6abe86a4bc0b5
Zeboto <zebrawii@gmail.com>
2020-12-29 17:00:39 -0500

    Add join reviewers command

--- cogs/utils.py
+++ cogs/utils.py
@@ -35,7 +35,7 @@ class Utils(commands.Cog):
                     await conn.fetch(
                         """
                         INSERT INTO scan_channels (channel_id, server_id)
-                            VALUES ($1, $2)
+                        VALUES ($1, $2)
                         """,
                         channel.id,
                         channel.guild.id
@@ -46,7 +46,33 @@ class Utils(commands.Cog):
                     pass
         
         
-    
+    @commands.check(check_granted_server)
+    @commands.command("join_review")
+    async def join_review_command(self, ctx: commands.Context):
+        db_utils = self.bot.get_cog('DBUtils')
+        if db_utils is None:
+            self.bot.logger.info("The cog \"DBUtils\" is not loaded")
+            return
+        db_check = self.bot.get_cog('DBChecks')
+        if db_check is None:
+            self.bot.logger.info("The cog \"DBChecks\" is not loaded")
+            return
+            
+        if await db_check.is_reviewer(ctx.author.id):
+            await ctx.send(f"{ctx.author.mention} You are already a reviewer.")
+            return
+
+        category_id = self.bot.config.get('reviewer_category')
+        category = [cat for cat in ctx.guild.categories if cat.id == category_id][0]
+        
+        channel = await category.create_text_channel(ctx.author.display_name.lower())
+        
+        await db_utils.add_reviewer(ctx.author.id, channel.id)
+
+        await ctx.author.add_roles(ctx.guild.get_role(self.bot.config.get('review_role')))
+
+        await ctx.send(f"{ctx.author.mention} You can start reviewing at {channel.mention}.")
+        
     @commands.is_owner()
     @commands.command("reload_config")
     async def reload_channel_command(self, ctx: commands.Context):
