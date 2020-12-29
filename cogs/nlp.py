eef1fff9a225493ce3e707b06c82f72a3c2af33b
Zeboto <zebrawii@gmail.com>
2020-12-29 12:11:02 -0500

    Update clean text function

--- cogs/nlp.py
+++ cogs/nlp.py
@@ -130,16 +130,15 @@ class NLP(commands.Cog):
         text = re.sub(r"<@&\d{15,21}>", "__role__", text) # Role mentions
         text = re.sub(r"<#\d{15,21}>", "__channel__", text) # Channel mentions
         
-        text = re.sub(r"what's", "what is ", text)
-        text = re.sub(r"\'s", " ", text)
-        text = re.sub(r"\'ve", " have ", text)
-        text = re.sub(r"can't", "cannot ", text)
-        text = re.sub(r"n't", " not ", text)
-        text = re.sub(r"i'm", "i am ", text)
-        text = re.sub(r"\'re", " are ", text)
-        text = re.sub(r"\'d", " would ", text)
-        text = re.sub(r"\'ll", " will ", text)
-        text = re.sub(r"\'scuse", " excuse ", text)
+        text = re.sub(r"what's", "what is", text)
+        text = re.sub(r"\'s", "", text)
+        text = re.sub(r"\'ve", " have", text)
+        text = re.sub(r"can't", "cannot", text)
+        text = re.sub(r"i'm", "i am", text)
+        text = re.sub(r"\'re", " are", text)
+        text = re.sub(r"\'d", " would", text)
+        text = re.sub(r"\'ll", " will", text)
+        text = re.sub(r"\'", "", text)
         text = re.sub('\W', ' ', text)
         text = re.sub('\s+', ' ', text)
         
