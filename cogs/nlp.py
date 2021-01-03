ae350bb5b20a5c808d0bff9b348b112fa1dcfe2d
Ian M <6500391+7bitlyrus@users.noreply.github.com>
2021-01-03 14:59:02 -0500

    Update precentage to use ints

--- cogs/nlp.py
+++ cogs/nlp.py
@@ -98,7 +98,7 @@ class NLP(commands.Cog):
 
             for i, (k, v) in enumerate(scores.items()):
                 score_val = round(v,2)
-                if v > self.bot.config.get('flag_threshold'): score_val = f'**{round(score_val,2) * 100}%**'
+                if v > self.bot.config.get('flag_threshold'): score_val = f'**{int(round(score_val * 100))}%**'
                 score_values.append(f"{self.bot.config.get('reaction_emojis')[i]} {score_val}")
 
             embed = discord.Embed(
